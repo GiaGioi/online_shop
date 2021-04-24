1270bc420470e0f805e99e6d7d7359afd339b8d6
GiaGioi <nguyengiagioi99@gmail.com>
2021-04-24 22:06:56 +0700

    update code

--- lib/pages/root_app.dart
+++ lib/pages/root_app.dart
@@ -8,12 +8,112 @@ class RootApp extends StatefulWidget {
 }
 
 class _RootAppState extends State<RootApp> {
+  int activeTab = 0;
+
+  AppBar appBar = null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       bottomNavigationBar: getFooter(),
+      appBar: getAppbar(),
+      body: getBody(),
     );
+  }
+
+  Widget getBody(){
+    return IndexedStack(
+      index: activeTab,
+      children: [
+        Center(
+          child: Text('Home',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        ),
+        Center(
+          child: Text('Store',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        ),
+        Center(
+          child: Text('Account',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        ),
+        Center(
+          child: Text('Cart',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        ),
+        Center(
+          child: Text('More',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        ),
+      ],
+    );
+
+  }
+  Widget getAppbar() {
+    switch (activeTab) {
+      case 0:
+        return AppBar(
+          elevation: 0.8,
+          backgroundColor: white,
+          title: Center(
+            child: Text(
+              'HOME',
+              style: TextStyle(color: black),
+            ),
+          ),
+        );
+        break;
+      case 1:
+        return AppBar(
+          elevation: 0.8,
+          backgroundColor: white,
+          title: Center(
+            child: Text(
+              'CART',
+              style: TextStyle(color: black),
+            ),
+          ),
+        );
+        break;
+      case 2:
+        return AppBar(
+          elevation: 0.8,
+          backgroundColor: white,
+          title: Center(
+            child: Text(
+              'ACCOUNT',
+              style: TextStyle(color: black),
+            ),
+          ),
+        );
+        break;
+      case 3:
+        return AppBar(
+          elevation: 0.8,
+          backgroundColor: white,
+          title: Center(
+            child: Text(
+              'CART',
+              style: TextStyle(color: black),
+            ),
+          ),
+        );
+        break;
+      case 4:
+        return AppBar(
+          elevation: 0.8,
+          backgroundColor: white,
+          title: Center(
+            child: Text(
+              'MORE',
+              style: TextStyle(color: black),
+            ),
+          ),
+        );
+        break;
+      default:
+    }
   }
 
   Widget getFooter() {
@@ -29,9 +129,16 @@ class _RootAppState extends State<RootApp> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: List.generate(itemsTab.length, (index) {
             return IconButton(
-                icon: Icon(itemsTab[index]['icon'],
-                    size: itemsTab[index]['size']),
-                onPressed: () {});
+                icon: Icon(
+                  itemsTab[index]['icon'],
+                  size: itemsTab[index]['size'],
+                  color: activeTab == index ? accent : black,
+                ),
+                onPressed: () {
+                  setState(() {
+                    activeTab = index;
+                  });
+                });
           }),
         ),
       ),
